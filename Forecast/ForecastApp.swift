15e20d90bf7d1e895e436f90864de79a11122ebd
haykct <https://github.com/HaykoHayrapetyan>
2023-07-30 20:23:41 +0200

    Added project files.

---
+++ Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Hayk Hayrapetyan on 30.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
